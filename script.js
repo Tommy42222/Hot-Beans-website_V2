b04f316d6a4ba37ae8b09b18de1bd3b73e40eb22
Tom Horsman <thomashorsman99@gmail.com>
2025-04-23 19:19:04 +0100

    pop-up application window and styling

---
+++ script.js
@@ -0,0 +1,11 @@
+function openform() {
+    document.getElementById("apply-form-container").style.display = "block";
+    document.getElementById("apply-button").style.display = "none"
+    document.getElementById("close-button").style.display = "block"
+}
+
+function closeform() {
+    document.getElementById("apply-form-container").style.display = "none"
+    document.getElementById("apply-button").style.display = "block"
+    document.getElementById("close-button").style.display = "none"
+}
